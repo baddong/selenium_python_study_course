ecc06509a239b2904dcd7faa96fabc77d469c0f0
a.seretyrov <seretyrov@gmail.com>
2019-07-21 12:59:19 +0300

    added basepage class

---
+++ pages/base_page.py
@@ -0,0 +1,8 @@
+ class BasePage(object): 
+	"""docstring for BasePage"""
+	def __init__(self, browser, url):
+		self.browser = browser
+		self.url = url
+	def open(self):
+		self.browser.get(self.url)	
+		
