859eafa3572a9e1136f92bd93b3110c1953d8542
a.seretyrov <seretyrov@gmail.com>
2019-07-24 19:18:07 +0300

    added locators.py

---
+++ pages/locators.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators(object):
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class ProductPageLocators(object):
+	PRODUCT_LINK = ()
